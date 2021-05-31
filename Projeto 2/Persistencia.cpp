bd9e7f239d80dc41102ef71d418303dd98873ece
Gabriel-Alexandre <biellalex86@gmail.com>
2021-05-31 15:06:33 -0300

    lerInsumos() atualizacao 1

--- Projeto 2/Persistencia.cpp
+++ Projeto 2/Persistencia.cpp
@@ -2,6 +2,33 @@
 	
 void Persistencia::lerInsumos(Controler *controle) {
 
+    std::ifstream file;
+    std::string linha, dado;
+
+    file.open("ArquivosInsumos.txt");
+    //carregar dados do arquivo para o objeto controler
+
+    if (file.is_open()){
+        while(getline(file, linha)) {
+            for(int i = 0; i < linha.size(); i++) {
+                if(linha[i] != ',' && i < linha.size()) {
+                   dado += linha[i];
+                }else{
+                   for(int j = 0; j < 28; j++){
+                       if(dado == "Ministério da Saúde"){
+                           //controle->cadastrarInsumosMS()
+                           //Preciso identificar qual o tipo de insumo
+
+                       }else{
+                           //controle->distribuirInsumo()
+                           //Preciso identificar qual o tipo de insumo
+
+                       }
+                   }
+                }
+            }
+        }
+    }
 }
 
 bool Persistencia::salvarInsumos(Controler controle) {
