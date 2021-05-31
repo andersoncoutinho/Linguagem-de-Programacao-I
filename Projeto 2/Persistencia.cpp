265a62a9eef5f74b5d98be0e5d7472e88a2be652
Gabriel-Alexandre <biellalex86@gmail.com>
2021-05-31 12:38:05 -0300

    Adicionando arquivos

--- Projeto 2/Persistencia.cpp
+++ Projeto 2/Persistencia.cpp
@@ -14,7 +14,7 @@ bool Persistencia::salvarInsumos(Controler controle) {
     if (file.is_open()){
 
         for (int i = 0; i < QTD_ESTADOS; i++) {
-            file << controle.getNomeLocal(i);
+            file << controle.getNomeLocal(i) << std::endl;
 
             for (int j = 0; j < controle.getLocal(i).getInsumos()->size(); j++) {
 
